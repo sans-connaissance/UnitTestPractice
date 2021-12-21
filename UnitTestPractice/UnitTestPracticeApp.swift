a1cc29c12a15649c26df565b51972771cdc7f66a
sans-connaissance <davemalicke@gmail.com>
2021-12-21 13:25:59 -0500

    Initial Commit

---
+++ UnitTestPractice/UnitTestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestPracticeApp.swift
+//  UnitTestPractice
+//
+//  Created by David Malicke on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
