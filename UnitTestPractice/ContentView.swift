399d754c88a0d03a094624789cad867ab611797d
sans-connaissance <davemalicke@gmail.com>
2021-12-21 15:37:09 -0500

    ADD simple tests to ensure textfields and buttons are present

--- UnitTestPractice/ContentView.swift
+++ UnitTestPractice/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var taskName: String = ""
+    @State private var tasks: [String] = []
+    @State private var isPresented: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List(tasks, id: \.self) { name in
+                    Text(name)
+                }.accessibility(identifier: "taskList")
+            }
+            .sheet(isPresented: $isPresented, onDismiss: {
+                tasks.append(taskName)
+            }, content: {
+                AddTaskScreen(taskTitle: $taskName)
+            })
+            .navigationTitle("Tasks")
+            .navigationBarItems(trailing: Button(action: {
+                isPresented = true
+            }, label: {
+                Image(systemName: "plus")
+                   
+            }).accessibility(identifier: "showAddTaskButton")
+            )
+        }
     }
 }
 
--- UnitTestPracticeUITests/UnitTestPracticeUITests.swift
+++ UnitTestPracticeUITests/UnitTestPracticeUITests.swift
@@ -7,36 +7,29 @@
 
 import XCTest
 
-class UnitTestPracticeUITests: XCTestCase {
+class when_add_new_task_screen_is_presented: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    var app: XCUIApplication!
+    
+    override func setUp() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let addTaskButton = app.buttons["showAddTaskButton"]
+        addTaskButton.tap()
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func test_make_sure_screen_is_displayed() {
+                
+        let addTaskNavBarTitle = app.staticTexts["Add Task"]
+        XCTAssert(addTaskNavBarTitle.waitForExistence(timeout: 0.5))
     }
+    
+    func test_ensure_all_required_controls_are_present() {
+    
+        XCTAssert(app.textFields["addTaskTextField"].exists)
+        XCTAssert(app.textFields["showAddTaskButton"].exists)
+        
+    }
+    
 }
